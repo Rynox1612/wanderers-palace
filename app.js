e72bd05266aa997b6ba41b1f4c8bdcd9eed09e30
Rupesh Sahu <rssahu8454@gmail.com>
2025-08-28 14:19:26 +0530

    styling part 2

--- app.js
+++ app.js
@@ -1,74 +1,76 @@
-const express=require("express");
-const app=express();
-const mongoose=require("mongoose");
-const Listing=require("./models/listing");
-const path=require("path");
-const methodOverride=require("method-override");
-const ejsMate=require("ejs-mate");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
-app.engine("ejs",ejsMate);
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views")); 
-app.use(express.static(path.join(__dirname,"public")));
-app.use(express.urlencoded({extended:true}));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-
-let MONGOOSE_URL= "mongodb://127.0.0.1:27017/WanderPalace";
+let MONGOOSE_URL = "mongodb://127.0.0.1:27017/WanderPalace";
 main()
-.then(()=>{
+  .then(() => {
     console.log("Connected to DB");
-})
-.catch((err)=>{
-    console.log(err)
-})
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-    mongoose.connect(MONGOOSE_URL);
+  mongoose.connect(MONGOOSE_URL);
 }
 
-app.listen("8080",()=>{
-    console.log("server is listening on port 8080");
+app.listen("8080", () => {
+  console.log("server is listening on port 8080");
 });
 
-app.get("/listings",async (req,res)=>{
-    let properties=await Listing.find();
-    res.render("index",{properties});
+app.get("/listings", async (req, res) => {
+  let properties = await Listing.find();
+  res.render("index", { properties });
 });
 
-app.post("/listings",async (req,res)=>{
-    let property=new Listing(req.body.listing);
-    property.save();
-    res.redirect("/listings")
+app.post("/listings", async (req, res) => {
+  let property = new Listing(req.body.listing);
+  property.save();
+  res.redirect("/listings");
 });
 
-app.get("/listings/new",async (req,res)=>{
-    res.render("new");
+app.get("/listings/new", async (req, res) => {
+  res.render("new");
 });
 
-app.get("/listings/:id",async (req,res)=>{
-    let {id}=req.params;
-    let property=await Listing.findById(id)
-    res.render("property.ejs",{property});
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let property = await Listing.findById(id);
+  res.render("property.ejs", { property });
 });
 
-app.get("/listings/:id/edit",async (req,res)=>{
-    let {id}=req.params;
-    let property=await Listing.findById(id);
-    res.render("edit", { property });
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let property = await Listing.findById(id);
+  res.render("edit", { property });
 });
 
-app.post("/listings/:id",async (req,res)=>{
-    let property=req.body.listing;
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,property, { new: true, runValidators: true });
-    
-    res.redirect(`/listings/${id}`);
+app.post("/listings/:id", async (req, res) => {
+  let property = req.body.listing;
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, property, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.redirect(`/listings/${id}`);
 });
 
-app.delete("/listings/:id",async (req,res)=>{
-    let {id}=req.params;
-    console.log(id);
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-})
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  console.log(id);
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+});
--- models/listing.js
+++ models/listing.js
@@ -1,31 +1,31 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-let Schema=mongoose.Schema;
+let Schema = mongoose.Schema;
 
-let listingSchema=new Schema({
-    title: {
-        type: String,
+let listingSchema = new Schema({
+  title: {
+    type: String,
+  },
+
+  description: {
+    type: String,
+  },
+
+  price: {
+    type: Number,
+  },
+
+  image: {
+    url: {
+      type: String,
     },
+  },
 
-    description: {
-        type: String,
-    },
-
-    price: {
-        type: Number,
-    },
-
-    image:{
-        url:{
-        type: String,
-    },
-    },
-
-    country: {
-        type: String
-    }
+  country: {
+    type: String,
+  },
 });
 
-const Listing=mongoose.model("Listing",listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 
-module.exports= Listing;
+module.exports = Listing;
